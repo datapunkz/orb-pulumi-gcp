bda1b0a6ee2fe0ce6d0391237e60fade044dd312
Angel Rivera <ariv3ra@gmail.com>
2019-04-02 22:40:33 -0400

    eliminating scopes for the stack

--- pulumi/gcp/gke/__main__.py
+++ pulumi/gcp/gke/__main__.py
@@ -39,9 +39,6 @@ cluster = container.Cluster(
             "https://www.googleapis.com/auth/devstorage.read_only",
             "https://www.googleapis.com/auth/logging.write",
             "https://www.googleapis.com/auth/monitoring",
-            "https://www.googleapis.com/auth/service.management.readonly",
-            "https://www.googleapis.com/auth/servicecontrol",
-            "https://www.googleapis.com/auth/trace.append",
         ],
     }
 )
