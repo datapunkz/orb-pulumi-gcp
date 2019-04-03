90d2bfe68c56658dd7e055356142d5f34167b076
Angel Rivera <ariv3ra@gmail.com>
2019-04-02 23:34:19 -0400

    ver 0007

--- hello_world.py
+++ hello_world.py
@@ -10,7 +10,7 @@ def generate_html(message):
                 <image height="340" width="1200" src="https://user-images.githubusercontent.com/194400/41597205-a57442ea-73c4-11e8-9591-61f5c83c7e66.png">
                 <br>
                 {0}<br>
-                <br><p>Version Update: 0006</p>
+                <br><p>Version Update: 0007</p>
             </div>
         </body>
         </html>""".format(message)
