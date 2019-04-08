1ebcc81c3d1cc137ce41f4dd09ed77c2e9e53399
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-04-08 05:17:50 -0700

    Pulumi GSP GE code ready for merge to master

--- hello_world.py
+++ hello_world.py
@@ -3,21 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 
 def generate_html(message):
+    version_number = '0001'
     html = """
         <html>
         <body>
             <div style='text-align:center;font-size:80px;'>
-                <image width="1200" height="500" src="https://storage.googleapis.com/gweb-cloudblog-publish/original_images/GC_NEXT-Blog-Header_V1.png">
-                <br>
-                {0}
-                <p>Version Number: 0000/p>
+                <image height="340" width="1200" src="https://user-images.githubusercontent.com/194400/41597205-a57442ea-73c4-11e8-9591-61f5c83c7e66.png">
+                <br> {0}
+                <p>Version Number: {1}</p>
             </div>
         </body>
-        </html>""".format(message)
+        </html>""".format(message,version_number)
     return html
 
 def greet():
-    greeting = 'Welcome to CI/CD 101 using CircleCI!'
+    greeting = 'Welcome to CI/CD'
     return greeting
 
 @app.route('/')
