1adbcc1e786dd051862e0fd163ed620f1300b929
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-04-09 09:25:26 -0700

    tagged version 0001

--- hello_world.py
+++ hello_world.py
@@ -3,16 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 def generate_html(message):
+    version_number = '0001'
     html = """
         <html>
         <body>
             <div style='text-align:center;font-size:80px;'>
                 <image width="800" height="333" src="https://storage.googleapis.com/gweb-cloudblog-publish/original_images/GC_NEXT-Blog-Header_V1.png">
                 <br> {0}
-                <p>Version Number: 0000 </p>
+                <p>Version Number: {1}</p>
             </div>
         </body>
-        </html>""".format(message)
+        </html>""".format(message,version_number)
     return html
 
 def greet():
