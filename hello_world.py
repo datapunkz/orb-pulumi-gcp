4e5871d06238aaddbc71da181ebdf6d9011bb98d
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-12-11 17:48:48 -0500

    update version

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 def generate_html(message):
-    version_number = '0001'
+    version_number = '0002'
     html = """
         <html>
         <body>
