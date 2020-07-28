ab401fb59eb83bee46a9be40154b9af1c9aa695d
Angel Rivera <ariv3ra@gmail.com>
2020-07-28 08:45:24 -0400

    changed the ver number

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 def generate_html(message):
-    version_number = '0012'
+    version_number = '0013'
     html = """
         <html>
         <body>
